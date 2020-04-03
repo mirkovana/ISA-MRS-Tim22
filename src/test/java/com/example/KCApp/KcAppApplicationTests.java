8064ad4168d868cbb599ac5590df1859b78ecba0
mirkovana <mana81098@gmail.com>
2020-04-03 19:53:27 +0200

    Inicijalni komit

---
+++ src/test/java/com/example/KCApp/KcAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KCApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KcAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
