f7f3389b01dab3946fdec5fcb2808984a4981652
marijakas <makyns98@gmail.com>
2020-05-26 15:32:39 +0200

    Lista pacijenata klinike kojoj medicinska sestra pripada

--- src/main/java/com/example/KCApp/repository/PregledRepository.java
+++ src/main/java/com/example/KCApp/repository/PregledRepository.java
@@ -1,9 +1,12 @@
 package com.example.KCApp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.KCApp.beans.Klinika;
 import com.example.KCApp.beans.Pregled;
 
 public interface PregledRepository extends JpaRepository<Pregled, Integer>{
-
+	List<Pregled> findAllByKlinika(Klinika K);
 }
