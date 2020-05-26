6338b4931a2f2d03341802502729b040c6507cfd
mirkovana <mana81098@gmail.com>
2020-05-26 13:47:32 +0200

    Ispravan rad funkcija za prikazivanje pregleda i operacija kod pacijenta

--- src/main/java/com/example/KCApp/controller/OperacijaController.java
+++ src/main/java/com/example/KCApp/controller/OperacijaController.java
@@ -1,11 +1,13 @@
 package com.example.KCApp.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,11 +23,22 @@ public class OperacijaController {
 	private OperacijaService service;
 	
 	/*ISPISIVANJE OPERACIJA*/
-	@GetMapping(value="/operacije")
+	@GetMapping(value="/operacije/{id}")
 	@PreAuthorize("hasRole('PACIJENT')")
-	public List<Operacija> getAllOperacije(Model model) {
+	public List<Operacija> getAllOperacije(Model model, @PathVariable Integer id) {
 		List<Operacija> listaOperacija = service.listAll();
-		model.addAttribute("listaOperacija", listaOperacija);
-		return listaOperacija;
+		for(Operacija o : listaOperacija)
+		{
+			System.out.println("USAO SAM U FOR");
+			System.out.println("OVO JE ID PACIJENTA " + o.getPacijent().getId());
+			if(id.equals(o.getPacijent().getId()))
+			{
+				System.out.println("USAO SAM U IF");
+				model.addAttribute("listaOperacija", listaOperacija);
+				return listaOperacija;
+			}
+		}
+		List<Operacija> prazna = new ArrayList<Operacija>();
+		return prazna;
 	}
 }
