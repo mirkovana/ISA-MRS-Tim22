95d2696792a8368211eff125d9a315f9a77cff6b
mirkovana <mana81098@gmail.com>
2020-05-26 14:40:02 +0200

    Prikaz zk pacijenta

--- src/main/java/com/example/KCApp/controller/ZdravstveniKartonController.java
+++ src/main/java/com/example/KCApp/controller/ZdravstveniKartonController.java
@@ -1,9 +1,13 @@
 package com.example.KCApp.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.KCApp.DTO.ZdravstveniKartonDTO;
+import com.example.KCApp.beans.Pregled;
 import com.example.KCApp.beans.ZdravstveniKarton;
 import com.example.KCApp.repository.ZdravstveniKartonRepository;
 import com.example.KCApp.service.ZdravstveniKartonService;
@@ -27,6 +32,22 @@ public class ZdravstveniKartonController {
 	
 	@Autowired
 	private ZdravstveniKartonRepository repository;
+	
+	@GetMapping(value="/kartoni/svi/{id}")
+	@PreAuthorize("hasRole('PACIJENT')")
+	public ZdravstveniKarton getAllZdravstveniKartoni(Model model, @PathVariable Integer id) {
+		List<ZdravstveniKarton> listaZdravstveniKartoni = service.listAll();
+		for(ZdravstveniKarton zk : listaZdravstveniKartoni)
+		{
+			if(id.equals(zk.getPacijent().getId()))
+			{
+				model.addAttribute("listaZdravstveniKartoni", listaZdravstveniKartoni);
+				return zk;
+			}
+		}
+		ZdravstveniKarton prazan = new ZdravstveniKarton();
+		return prazan;
+	}
 	
 	@GetMapping(value = "/kartoni/{idZdravstvenogKartona}")
 	public ZdravstveniKarton findZDById(@PathVariable Integer idZdravstvenogKartona) {
